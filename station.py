dc8f5b7bfecf4a9207e7ff71d5797db0e0ecca7f
Stathis Sidiropoulos <efstsidiropoulos@gmail.com>
2022-03-10 20:50:47 +0100

    Add methods to Station.

--- station.py
+++ station.py
@@ -7,32 +7,26 @@ class Station():
     def __str__(self):
         return str(self.id)
 
-    def set_delay(self, delay):
-        self.delay = delay
-
-    def set_station_north(self, next_station_north):
-        self.next_station_north = next_station_north
-
-    def set_station_south(self, next_station_south):
-        self.next_station_south = next_station_south
-
-    def set_line(self, line):
-        self.line = line
-
     def get_id(self):
         return self.id
     
     def get_delay(self):
         return self.delay
 
-    def get_line(self):
-        return self.line
-
-    def get_next(self, direction = "S"):
-        if direction.upper() == "S":
-            return self.next_station_south
-        else:
-            return self.next_station_north
-
     def __hash__(self):
         return hash(self.id)
+
+    def get_next_station(self, line, direction):
+        pass
+    
+    def set_connections(self, connections):
+        self.connections = connections
+
+    def get_connections(self):
+        return self.connections
+
+    def add_connection(self, connection):
+        pass
+
+    def remove_connection(self):
+        pass
