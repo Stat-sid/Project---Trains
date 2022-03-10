76f1f6e20dea62668a25577b39e64f7a773ff5cd
Stathis Sidiropoulos <efstsidiropoulos@gmail.com>
2022-03-10 21:34:56 +0100

    Add get_next_station and connection methods.

--- station.py
+++ station.py
@@ -3,9 +3,13 @@ class Station():
     def __init__(self, id, delay = 0):
         self.id = id
         self.delay = delay
+        self.connections = []
         
     def __str__(self):
         return str(self.id)
+
+    def __hash__(self):
+        return hash(self.id)
 
     def get_id(self):
         return self.id
@@ -13,20 +17,19 @@ class Station():
     def get_delay(self):
         return self.delay
 
-    def __hash__(self):
-        return hash(self.id)
-
     def get_next_station(self, line, direction):
-        pass
+        next_station = filter(
+            lambda c: c.get_to()
+                if c.get_line()==line and c.get_direction()==direction
+                else None
+            , self.connections)
+        return next_station
     
-    def set_connections(self, connections):
-        self.connections = connections
-
     def get_connections(self):
         return self.connections
 
     def add_connection(self, connection):
-        pass
+        self.connections.append(connection)
 
-    def remove_connection(self):
-        pass
+    def remove_connection(self, connection):
+        self.connections.remove(connection)
