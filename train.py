54aa8aff8079edba556527a14b2414628e083b38
Stathis Sidiropoulos <efstsidiropoulos@gmail.com>
2022-03-12 21:49:20 +0100

    Change move method logic, add supporting methods.

--- train.py
+++ train.py
@@ -1,3 +1,5 @@
+from random import random
+
 class Train():
         
     def __init__(self, id, line, direction, current_station) -> None:
@@ -9,5 +11,30 @@ class Train():
     def current_position(self):
         return [self.current_station, self.direction, self.line]
 
+    # I know this will not work right for a station without connections.
     def move(self):
-        self.current_station.get_next_station()
+        if self.__am_I_delayed():
+            pass
+        else:
+            if self.__get_next_station() is None:
+                self.__turn_around()
+            
+            next_station = self.__get_next_station()
+            self.current_station = next_station
+
+    def __am_I_delayed(self):
+        delay_probability = self.current_station.get_delay()
+        if random.random() < delay_probability:
+            return True
+        else:
+            return False
+
+
+    def __get_next_station(self):
+        return self.current_station.get_next_station(self.line, self.direction)
+
+    def __turn_around(self):
+        if self.direction == "N":
+            self.direction = "S"
+        elif self.direction == "S":
+            self.direction = "N"
