1b73822219cf94fe68e3a81c30a865dd9fac250c
Stathis Sidiropoulos <efstsidiropoulos@gmail.com>
2022-03-19 12:49:02 +0100

    Add BFS method to world.

--- world.py
+++ world.py
@@ -1,3 +1,4 @@
+
 class World():
 
     def __init__(self, stations, trains):
@@ -5,14 +6,16 @@ class World():
         self.trains = trains
 
     def get_station(self, id):
-        for s in self.stations:
-            if s.get_id() == id:
-                return s
+        for station in self.stations:
+            if station.get_id() == id:
+                return station
+        raise KeyError
 
     def get_train(self, id):
         for train in self.trains:
             if train.get_id() == id:
                 return train
+        raise KeyError
 
     def get_stations(self):
         return self.stations
@@ -31,16 +34,38 @@ class World():
         queue = []
         visited.append(_from)
         queue.append(_from)
-        
-        while queue:
+          
+        while queue:     
             m = queue.pop(0)
-            print(m, end = " ")
+            # print(m, end = " ")
 
             for connection in m.get_connections():
                 if connection.get_to() not in visited:
                     visited.append(connection.get_to())
                     queue.append(connection.get_to())
+        return visited
 
+
+    def get_shortest_path(self, start, end, path=[]):
+        """
+        __source__='https://www.python.org/doc/essays/graphs/'
+        __author__='Guido van Rossum'
+        Adapted by: Efstathios Sidiropoulos.
+        """
+        path = path + [start]
+        if start == end:
+            return path
+        if start not in self.stations:
+            return None
+        shortest = None
+
+        for connection in start.get_connections():
+            if connection.get_to() not in path:
+                newpath = self.get_shortest_path(connection.get_to(), end, path)
+                if newpath:
+                    if not shortest or len(newpath) < len(shortest):
+                        shortest = newpath
+        return shortest
 
 
 
